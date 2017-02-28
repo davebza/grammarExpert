8c1dea311411883e21ca967d8990d38a41b1b175
Dave Brown <daveb.za@gmail.com>
2017-02-28 17:11:44 +0800

    Complete: P5 data JSON - questions and answers, answers are at position [0] of array

---
+++ data.js
@@ -0,0 +1,28 @@
+/* This file is for the data objects and procedures to retrieve from those objects
+for the English Day activity */
+
+var questionsP5 = [{
+	"question": "Ms. Chan _____________ a concert in England two years ago.",
+	"answers": ["watched", "have watched", "has watched", "is watching"]
+}, {
+	"question": "Mr and Mrs Chan __________ their favourite movie for two hours.",
+	"answers": ["watched", "have watch", "have watched", "has watched"]
+}, {
+	"question": "Maggie ___________ on stage since 1995.",
+	"answers": ["has sung", "sang", "has sang", "sung"]
+}, {
+    "question" : "My classmate Susan ___________ a prefect for 3 months.",
+    "answers" : ["has been", "did", "has done", "became"]
+}, {
+    "question" : "Tom has been an animator ______ 1989.",
+    "answers" : ["since", "for", "in", "ago"]
+}, {
+    "question" : "Mum asked '______ you ______ your lunch yet?'",
+    "answers" : ["have...had", "did...have", "did...had", "have...have"]
+}, {
+    "question" : "Kelly won the best singer award ____ 2007.",
+    "answers" : ["in", "since", "for", "ago"]
+}, {
+    "question" : "Betty decided to become an athlete 24 years ______.",
+    "answers" : ["ago", "since", "for", "in"]
+}]
